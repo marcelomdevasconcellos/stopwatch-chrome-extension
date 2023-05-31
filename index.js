45e9daeb805397958a8c4ec2ab4ad9e6105b8b63
Marcelo Vasconcellos <marcelomdevasconcellos@gmail.com>
2023-05-31 18:37:44 -0400

    Update index.js

--- index.js
+++ index.js
@@ -38,12 +38,34 @@ function displayStopWatch(){
 
 
 function stopWatch(){
+    alert("Hello! I am an alert box!!");
     enableStopWatch = false;
 }
 
 function resetStopWatch(){
     seconds = mins = hours = 0;
     displayStopWatch();
+
 }
 
 resetStopWatch();
+
+function startTimer(duration, display) {
+    var timer = duration, minutes, seconds;
+    setInterval(function () {
+        minutes = parseInt(timer / 60, 10);
+        seconds = parseInt(timer % 60, 10);
+        minutes = minutes < 10 ? "0" + minutes : minutes;
+        seconds = seconds < 10 ? "0" + seconds : seconds;
+        display.textContent = minutes + ":" + seconds;
+        if (--timer < 0) {
+            timer = duration;
+        }
+    }, 1000);
+}
+window.onload = function () {
+    var duration = 60 * 5; // Converter para segundos
+        display = document.querySelector('#timer'); // selecionando o timer
+    startTimer(duration, display); // iniciando o timer
+
+};
